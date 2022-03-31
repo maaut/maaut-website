ba901e04538dbb8835aa4228a22bc977b52f0ffd
Mauricio Torres <maauricio.torres@gmail.com>
2022-03-31 14:52:20 -0300

    created coming soon page

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
